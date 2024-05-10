25e5e96e4c3b9d8a5b58744e13bb95a704b50290
DESKTOP-JE8V5I0\JellonaVesa <vesaruntti@gmail.com>
2024-05-10 14:54:11 +0300

    App.tsx - Added redux provide implementation. - Toast modal implementation. - Added fonts initialization.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,15 +3,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import HomeScreen from './src/screens/HomeScreen';
+import { store } from './src/store/store';
+import { Provider } from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
 export const App = () => {
-
   // Load Fonts at the start
   const [fontsLoaded, fontError] = useFonts({
     'Poppins-Thin': require('./assets/fonts/Poppins-Thin.ttf'),
     'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
+    'Poppins-ExtraLight': require('./assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
   });
 
   // TODO: Handle if fonts are not loaded
@@ -20,13 +25,25 @@ export const App = () => {
   }
 
   return (
-    <RootSiblingParent>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='Home' component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </RootSiblingParent>
+    <Provider store={store}>
+      <RootSiblingParent>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShadowVisible: false,
+              headerTransparent: true,
+            }}>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                title: '',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </RootSiblingParent>
+    </Provider>
   );
 };
 
