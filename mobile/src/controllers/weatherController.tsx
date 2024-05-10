342a13bcfe98e65e914f2c3f2b5e4d924ecd983b
DESKTOP-JE8V5I0\JellonaVesa <vesaruntti@gmail.com>
2024-05-10 14:50:55 +0300

    - Weather controller updated

--- mobile/src/controllers/weatherController.tsx
+++ mobile/src/controllers/weatherController.tsx
@@ -2,55 +2,62 @@ import Toast from 'react-native-root-toast';
 const WEAHER_API_URL = process.env.EXPO_PUBLIC_WEATHER_API_URL;
 const WEAHER_API_KEY = process.env.EXPO_PUBLIC_WEATHER_API_KEY;
 
-export const fetchWeather = async (searchText: string) => {
-    // Check if search text  has valid lenght
-    if (searchText.length <= 0) {
-        Toast.show(`Search fields are empty`, {
-            duration: Toast.durations.LONG,
-        });
-        return;
-    }
-    
-    // Declar request url
-    const url: URL = new URL(WEAHER_API_URL!);
+export const fetchWeather = async (text: string) => {
+  // Check if search text  has valid lenght
+  if (text.length <= 0) {
+    Toast.show(`Search field is empty`, {
+      duration: Toast.durations.LONG,
+    });
+    return;
+  }
 
-    // Declare search parameters
-    const searchParams: Record<string, any> = new URLSearchParams();
+  // Declare request url
+  const url: URL = new URL(WEAHER_API_URL!);
 
-    // Append search parameters
-    searchParams.append('key', WEAHER_API_KEY!);
-    searchParams.append('q', searchText!);
+  // Declare search parameters
+  const searchParams: Record<string, any> = new URLSearchParams();
 
-    // Assign search params to url search
-    url.search = new URLSearchParams(searchParams).toString();
+  // Append search parameters
+  searchParams.append('key', WEAHER_API_KEY!);
+  searchParams.append('q', text!);
+  searchParams.append('days', 5);
 
-    // Declare fetch options
-    const options: RequestInit = {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        credentials: 'same-origin',
-    };
+  // Assign search params to url search
+  url.search = new URLSearchParams(searchParams).toString();
 
-    await fetch(url, options)
-        .then(response => {
-            if (!response.ok) {
-                const errorMessage = `Request failed: ${response.status}`;
-                Toast.show(errorMessage, {
-                    duration: Toast.durations.LONG,
-                });
-            }
-            return response;
-        })
-        .then(async returnedResponse => {
-            const weatherData = await returnedResponse.json();
-            console.log(weatherData);
-        })
-        .catch(err => {
-            Toast.show(`Request failed: ${err}`, {
-                duration: Toast.durations.LONG,
-            });
-        });
+  // Declare fetch options
+  const options: RequestInit = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'same-origin',
+  };
+
+  const response: Response = await fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        return response;
+      }
+      return response;
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      return err;
+    });
+
+  // Check response errors more information
+  // https://www.weatherapi.com/docs/  ->  API Error Coder
+  if (response.status >= 400) {
+    const errorMessage = `Request failed: ${response.status}`;
+    Toast.show(errorMessage, {
+      duration: Toast.durations.LONG,
+    });
+    return;
+  }
+
+  return await response.json();
 };
